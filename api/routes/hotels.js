fd3bcee0c1df1ab1646b0cd3d7d6143e40bbf501
hosein <hosein@lap-007.hasingroup.com>
2022-05-30 17:00:00 +0430

    feat:add admin guard for some hotel routes

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -7,17 +7,19 @@ import {
   getHotelById,
   updatedHotel,
 } from '../controllers/hotel.js';
+// Utilities
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 // Create
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
 
 // Update
-router.put('/:id', updatedHotel);
+router.put('/:id', verifyAdmin, updatedHotel);
 
 // Delete
-router.delete('/:id', deleteHotel);
+router.delete('/:id', verifyAdmin, deleteHotel);
 
 // Get By ID
 router.get('/:id', getHotelById);
