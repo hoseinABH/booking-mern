5965085430c33c468b9372fc4f8ad4c492882076
hosein <hosein@lap-007.hasingroup.com>
2022-05-31 12:37:49 +0430

    feat:implement data for FeaturedProperties section

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+// Components
+import { Link } from 'react-router-dom';
 // Styles
 import './navbar.css';
 
@@ -5,7 +7,9 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">BOOKING MERN</span>
+        <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
+          <span className="logo">BOOKING MERN</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
